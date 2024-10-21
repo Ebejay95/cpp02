dfbaab943de04cf797d91b03756baf46b035ce30
Jonathan Eberle <jeberle@student.42heilbronn.de>
2024-10-21 15:07:55 +0200

    towards_a_more_useful_fixed-point_number_class

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -5,22 +5,27 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: jeberle <jeberle@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/10/20 18:41:37 by jeberle           #+#    #+#             */
-/*   Updated: 2024/10/21 12:30:12 by jeberle          ###   ########.fr       */
+/*   Created: 2024/10/21 12:36:23 by jeberle           #+#    #+#             */
+/*   Updated: 2024/10/21 14:42:02 by jeberle          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./Fixed.hpp"
-#include <iostream>
 
-int	main( void )
+int main( void )
 {
 	Fixed a;
-	Fixed b( a );
-	Fixed c;
-	c = b;
-	std::cout << a.getRawBits() << std::endl;
-	std::cout << b.getRawBits() << std::endl;
-	std::cout << c.getRawBits() << std::endl;
+	Fixed const b( 10 );
+	Fixed const c( 42.42f );
+	Fixed const d( b );
+	a = Fixed( 1234.4321f );
+	std::cout << "a is " << a << std::endl;
+	std::cout << "b is " << b << std::endl;
+	std::cout << "c is " << c << std::endl;
+	std::cout << "d is " << d << std::endl;
+	std::cout << "a is " << a.toInt() << " as integer" << std::endl;
+	std::cout << "b is " << b.toInt() << " as integer" << std::endl;
+	std::cout << "c is " << c.toInt() << " as integer" << std::endl;
+	std::cout << "d is " << d.toInt() << " as integer" << std::endl;
 	return 0;
 }
